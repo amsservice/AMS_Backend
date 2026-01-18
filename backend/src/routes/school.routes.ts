9d01f202c3431f4345e81bb915c658d091a9031a
Piyush <raunaksingh4099@gmail.com>
2026-01-19 03:19:00 +0530

    Added proper validation inside the edit modal of school details in Principal dashboard

--- backend/src/routes/school.routes.ts
+++ backend/src/routes/school.routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { getMySchool, updateMySchool,getSchoolByCode } from '../controllers/school.controller';
 import { authMiddleware } from '../middleware/auth.middleware';
 import { allowRoles } from '../middleware/role.middleware';
+import { validate } from '../middleware/validate.middleware';
+import { updateSchoolSchema } from '../config/zod.schema';
 
 const router = Router();
 
@@ -10,7 +12,7 @@ router.get('/by-code/:code', getSchoolByCode);
 router.use(authMiddleware, allowRoles(['principal', 'teacher', 'student']));
 
 router.get('/me', getMySchool);
-router.put('/me', updateMySchool);
+router.put('/me', validate(updateSchoolSchema), updateMySchool);
 
 
 export default router;
